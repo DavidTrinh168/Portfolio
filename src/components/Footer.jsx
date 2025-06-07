463be5954ec82543ef3eaa1a08d073c1e40adc7e
Trinh Ngoc Dai <daingoctrinh72711@gmail.com>
2025-06-07 13:17:02 +0700

    Adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,19 @@
-export const Footer = () => {
+    import { ArrowUp } from "lucide-react";
 
-}
+    export const Footer = () => {
+      return (
+        <footer className="py-3 px-4 bg-card relative border-t border-border mt-12 flex flex-wrap justify-between items-center">
+          {" "}
+          <p className="text-sm text-muted-foreground">
+            {" "}
+            &copy; {new Date().getFullYear()} DavidTech.co. All rights reserved.
+          </p>
+          <a
+            href="#hero"
+            className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
+          >
+            <ArrowUp size={20} />
+          </a>
+        </footer>
+      );
+    };
